4f9e0e4c68601ad5636819221e22a03b503db8c3
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-07 11:35:53 -0400

    Fix missing key prop errors

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./Orders.css";
 
 const Orders = ({orders}) => {
-  const orderEls = orders.map((order) => {
+  const orderEls = orders.map((order, i) => {
     return (
-      <div className="order">
+      <div key={`${order.name}${i}`} className="order">
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map((ingredient) => {
-            return <li>{ingredient}</li>;
+            return <li key={`${ingredient}${i}`}>{ingredient}</li>;
           })}
         </ul>
       </div>
